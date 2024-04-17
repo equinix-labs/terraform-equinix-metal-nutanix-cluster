ac00c671d2e64cee78a16a35c976bcde85fb8d59
Chris Privitere <23177737+cprivitere@users.noreply.github.com>
2024-04-17 16:26:01 +0000

    feat: Use CVM dhcp name and check leases file
    Signed-off-by: Chris Privitere <23177737+cprivitere@users.noreply.github.com>

--- scripts/dhcp-check.sh
+++ scripts/dhcp-check.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #TODO: change the ip address and -ge # to be templated fields
-until [ "$(arp -a | grep "192.168.100" -c)" -ge 2 ]
+until [ "$(grep -c "CVM" /var/lib/misc/dnsmasq.leases)" -ge 1 ]
 do
         sleep 5
 done

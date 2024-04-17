68147e351fbe783f41dacfafa3f747b0a0882785
ocobleseqx <oscar.cobles@eu.equinix.com>
2024-04-17 23:40:57 +0200

    fmt
    Signed-off-by: ocobleseqx <oscar.cobles@eu.equinix.com>

--- scripts/firstboot-check.sh
+++ scripts/firstboot-check.sh
@@ -4,15 +4,14 @@
 timeout=1800
 
 counter=0
-until [ -e /root/.firstboot_success ] || [ "$counter" -ge "$timeout" ]
-do
-    sleep 5
-    counter=$((counter+5))
+until [ -e /root/.firstboot_success ] || [ "$counter" -ge "$timeout" ]; do
+	sleep 5
+	counter=$((counter + 5))
 done
 
 if [ "$counter" -ge "$timeout" ]; then
-    echo "Timeout reached waiting for the firstboot of the node to succeed."
-    exit 1
+	echo "Timeout reached waiting for the firstboot of the node to succeed."
+	exit 1
 fi
 
 echo "Firstboot succeeded!"

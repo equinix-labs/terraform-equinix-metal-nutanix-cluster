f47d5f50c9e9e863fcb0256855a3e7142c25c6b4
Chris Privitere <23177737+cprivitere@users.noreply.github.com>
2024-04-18 20:21:15 +0000

    fix: remove debug statements from create-cluster
    Signed-off-by: Chris Privitere <23177737+cprivitere@users.noreply.github.com>

--- scripts/create-cluster.sh
+++ scripts/create-cluster.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
 IPADDRS=$(awk '/CVM/ { if (ips != "") ips = ips ","; ips = ips $3 } END { print ips }' /var/lib/misc/dnsmasq.leases)
-echo $IPADDRS
 SSH_HOST=$(echo "$IPADDRS" | cut -f1 -d',')
-echo $SSH_HOST
 sshpass -p "Nutanix.123" ssh -t -o StrictHostKeyChecking=no admin@"$SSH_HOST" "echo \"Nutanix.123\" | sudo -S cluster --skip_discovery -s $IPADDRS create"

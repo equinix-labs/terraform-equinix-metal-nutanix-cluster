7293e92afc6270775dff418238946a2d84c8109e
Ayush Rangwala <ayush.rangwala@gmail.com>
2024-07-15 20:33:53 +0530

    create setup prism cluster scrit

--- examples/cluster-with-ad/main.tf
+++ examples/cluster-with-ad/main.tf
@@ -52,6 +52,34 @@ resource "equinix_metal_port" "ad_server_bond0" {
   vlan_ids = [local.vlan_id]
 }
 
+resource "null_resource" "setup_prism_central" {
+  depends_on = [equinix_metal_device.ad-server]
+
+  connection {
+    host        = module.nutanix-cluster.bastion_public_ip
+    private_key = chomp(module.nutanix-cluster.ssh_private_key_contents)
+    type        = "ssh"
+    user        = "root"
+  }
+
+  provisioner "file" {
+    destination = "/root/setup-prism-central.sh"
+    content = templatefile("${path.module}/setup-prism-central.sh.tmpl", {
+      PRISM_IP           = module.nutanix-cluster.prism_central_ip_address
+      USERNAME         = "admin"
+      DEFAULT_PASSWORD     = "Nutanix/4u)"
+      NEW_PASSWORD      ="A@yush17"
+      VIRTUAL_IP        ="192.168.103.254"
+      ISCSI_IP          ="192.168.103.253"
+      NTP_SERVER        ="0.north-america.pool.ntp.org"
+    })
+  }
+
+  provisioner "remote-exec" {
+    inline = ["/bin/sh /root/setup-prism-central.sh"]
+  }
+}
+
 resource "null_resource" "bastion_ssh" {
   depends_on = [equinix_metal_device.ad-server]
 
@@ -66,9 +94,10 @@ resource "null_resource" "bastion_ssh" {
     destination = "/root/configure-ad.sh"
     content = templatefile("${path.module}/configure-ad.sh.tmpl", {
       PRISM_IP           = module.nutanix-cluster.prism_central_ip_address
-      PRISM_USER         = "admin"
+      PRISM_USERNAME         = "admin"
       PRISM_PASSWORD     = "Nutanix/4u)"
-      AD_DOMAIN          = equinix_metal_device.ad-server.access_public_ipv4
+      AD_DOMAIN          = "equinixad.com"
+      AD_DOMAIN_IP          = equinix_metal_device.ad-server.access_public_ipv4
       AD_USERNAME        = "admin"
       AD_PASSWORD        = "Equinix@AD"
     })
